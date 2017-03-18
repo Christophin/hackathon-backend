f6fdebc88311e86e7b7402b07d57a158cf003344
Chris Coralli <ccoralli@gmail.com>
2017-03-18 15:38:27 -0400

    added user info to Photos.

--- server/controllers/photo.js
+++ server/controllers/photo.js
@@ -15,9 +15,10 @@ module.exports = {
     },
     getPhotos (req, res)    {
         Photo.findAll({
-            include:    {
-                model: Comment
-            },
+            include: [
+                { model: Users, attributes: ['username', 'profileUrl'] },
+                { model: Comments }
+            ],
             order: [['createdAt',  'DESC']]
         }).then(photos => res.status(201).send(photos))
             .catch(error => res.status(401).send(error));
