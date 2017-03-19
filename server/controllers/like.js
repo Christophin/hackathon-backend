0fe3ef532382a3552be9601ff8c1e0d5bda61f8a
Chris Coralli <ccoralli@gmail.com>
2017-03-18 21:59:15 -0400

    stuff

--- server/controllers/like.js
+++ server/controllers/like.js
@@ -11,5 +11,19 @@ module.exports =  {
         })
             .then(like => res.status(201).send(like))
             .catch(error => res.status(400).send(error));
+    },
+    getLike (req, res)  {
+        Like.findOne({
+            where:  {
+                photoId: req.query.photoId,
+                userId: req.query.userId
+            }
+        })
+            .then(resp => {
+                let likesCount = Object.keys(resp).length;
+                res.send(likesCount);
+                //res.status(200).send(resp)
+            })
+            .catch(error => res.status(400).send(error));
     }
 };
